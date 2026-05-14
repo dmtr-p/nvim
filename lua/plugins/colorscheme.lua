b806b4e6995debba47436ab9d606b11763ce31bb
Dmytro Protsyk <16881205+dmtr-p@users.noreply.github.com>
2026-05-14 11:39:46 +0300

    fix theme and updates

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,7 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { flavour = "latte" } },
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "catppuccin" },
+  },
+}
